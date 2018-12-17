c505a2e70ec8b48b01009a97cc523b63262ee5c2
hmt <dev@hmt.im>
2018-12-17 23:43:11 +0100

    erstelle wirklich alle Pfade beim Start

--- src-electron/main-process/configstore.js
+++ src-electron/main-process/configstore.js
@@ -1,7 +1,7 @@
 import { api } from 'electron-util'
 import Store from 'electron-store'
-import { mkdirSync } from 'fs'
-import { join } from 'path'
+import { mkdirSync, existsSync } from 'fs'
+import { join, dirname } from 'path'
 import { hostname } from 'os'
 
 api.app.setName('schild.report')
@@ -15,8 +15,8 @@ const configFile = new Store({
       dokument: { width: 1800, height: 800 }
     },
     plugins: {
-      source: join(api.app.getPath('documents'), api.app.getName(), 'vorlagen'),
-      destination: join(api.app.getPath('userData'), 'plugins'),
+      source: join(api.app.getPath('documents'), api.app.getName(), 'vorlagen', '.'),
+      destination: join(api.app.getPath('userData'), '.'),
       remoteRepos: ''
     },
     db: {},
@@ -24,8 +24,17 @@ const configFile = new Store({
   }
 })
 
+function ensureDirectoryExistence (filePath) {
+  const dir = dirname(filePath)
+  if (existsSync(dir)) {
+    return true
+  }
+  ensureDirectoryExistence(dir)
+  mkdirSync(dir)
+}
+
 console.log('Verzeichnisse anlegen oder verwenden …')
-try { mkdirSync(configFile.get('plugins.source')) } catch (err) { /* console.log(err) */ }
-try { mkdirSync(configFile.get('plugins.destination')) } catch (err) { /* console.log(err) */ }
+ensureDirectoryExistence(configFile.get('plugins.source'))
+ensureDirectoryExistence(configFile.get('plugins.destination'))
 
 export default configFile
