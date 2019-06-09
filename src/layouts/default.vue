664c1508105762a58da58e09e3df5a1699061861
hmt <dev@hmt.im>
2019-06-09 13:52:21 +0200

    svelte als dateinmae zulassen

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -76,7 +76,7 @@
             @mouseleave="showIcon = false"
             dense
           >
-            <q-item-section>{{dokument.slice(0, -5)}}</q-item-section>
+            <q-item-section>{{parse(dokument)}}</q-item-section>
             <q-item-section
               avatar
               v-if="repo === $route.params.repo && dokument === $route.params.id"
@@ -165,6 +165,9 @@ export default {
     }
   },
   methods: {
+    parse (filename) {
+      return parse(filename).name
+    },
     updateFolderStates () {
       this.$store.commit('data/updateConfigData', { folderStates: this.folderStates })
     },
