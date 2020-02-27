0031ced9c61b1c3cda492516c3079cd46b32db69
hmt <dev@hmt.im>
2020-02-27 20:18:49 +0100

    version.js angepasst

--- src/version.js
+++ src/version.js
@@ -1,2 +1,1 @@
-// in appveyor.yml verwendete env vars
-export const VERSION = { buildID: '%APPVEYOR_BUILD_ID%', buildVersion: '%APPVEYOR_BUILD_VERSION%', buildNo: '%APPVEYOR_BUILD_NUMBER%', gitHash: '%APPVEYOR_REPO_COMMIT%', gitMessage: '%APPVEYOR_REPO_COMMIT_MESSAGE%', production: false }
+export const VERSION = { buildVersion: 'development', gitHash: 'development', production: false }
