e63cd2651fc0935f7b8a50ae5664a84cc20207e7
hmt <dev@hmt.im>
2020-09-22 19:00:38 +0200

    nutze rollups watcher

--- src/components/Dokument.svelte
+++ src/components/Dokument.svelte
@@ -1,16 +1,39 @@
 <script>
   import { rollup } from "./App.svelte";
   import * as Comlink from "comlink";
-  import { configData, component, set_edit, set_mark, error, repo, dokument,
-  plugin, plugin_entry, klasse, selected, jahr, abschnitt,
-  kommentar, pdf_name, generic_pdf, webview, compiled_module,
-  warten } from './../stores.js';
-  import { join } from 'path'
+  import {
+    configData,
+    component,
+    set_edit,
+    set_mark,
+    error,
+    repo,
+    dokument,
+    plugin,
+    plugin_entry,
+    klasse,
+    selected,
+    jahr,
+    abschnitt,
+    kommentar,
+    pdf_name,
+    generic_pdf,
+    webview,
+    compiled_module,
+    warten,
+  } from "./../stores.js";
+  import { join } from "path";
 
-  export let schule
-  function callback() {
-    console.log("Modul wurde modifiziert...");
-    run_rollup();
+  export let schule;
+
+  function callback(ret) {
+    if (ret.code === "ERROR") {
+      $error = ret.error
+      return
+    } else {
+      $compiled_module = ret;
+      set_repo();
+    }
   }
   export async function run_rollup(args) {
     $warten = true;
@@ -23,29 +46,31 @@
             debug: $configData.debug,
             write: $configData.write,
             source_maps: $configData.source_maps,
-            plugin: args.file.startsWith("plugin")
+            plugin: args.file.startsWith("plugin"),
           }
         : null;
     try {
       await rollup.set_options(options);
-    } catch (error) { console.log(error)}
+    } catch (error) {
+      console.log(error);
+    }
     try {
-      $compiled_module = await rollup.build();
+      $compiled_module = await rollup.build(Comlink.proxy(callback));
       if (args) {
         $component = null;
         $dokument = args.file;
         $repo = args.repo;
       }
-      set_repo()
     } catch (error) {
       console.log(error);
     }
     $warten = false;
-    await rollup.watch(Comlink.proxy(callback));
   }
 
   $: props = {
-    componentPath: !$plugin ? join($configData.userData, 'bundle.js') : join($plugin || '', $plugin_entry || ''),
+    componentPath: !$plugin
+      ? join($configData.userData, "bundle.js")
+      : join($plugin || "", $plugin_entry || ""),
     compiled_module: $compiled_module,
     debug: $configData.debug,
     svelteProps: {
@@ -55,77 +80,80 @@
       jahr: $jahr,
       abschnitt: $abschnitt,
       privat: $configData.privateDaten,
-      knexConfig: $configData.db
-    }
-  }
-  $: if ($component) set_destroy()
-  $: props && set_props()
+      knexConfig: $configData.db,
+    },
+  };
+  $: if ($component) set_destroy();
+  $: props && set_props();
 
-  async function set_destroy () {
-    $plugin = null
-    $webview && await $webview.send('destroy')
+  async function set_destroy() {
+    $plugin = null;
+    $webview && (await $webview.send("destroy"));
   }
-  async function set_props () {
-    if ($component || !$webview) return
-    $webview.send('props', props)
+  async function set_props() {
+    if ($component || !$webview) return;
+    $webview.send("props", props);
   }
-  async function set_dokument () {
-    await $webview.send('props', props)
-    $webview.send('set_dokument')
-    $set_mark = true
-    $set_edit = false
-    $webview.removeEventListener('dom-ready', set_dokument)
+  async function set_dokument() {
+    await $webview.send("props", props);
+    $webview.send("set_dokument");
+    $set_mark = true;
+    $set_edit = false;
+    $webview.removeEventListener("dom-ready", set_dokument);
   }
-  async function set_repo () {
-    $error = null
-    if ($component) return
-    const base_url = `file2://${$plugin ? join($plugin) : join($configData.reports, $repo)}/`
+  async function set_repo() {
+    $error = null;
+    if ($component) return;
+    const base_url = `file2://${
+      $plugin ? join($plugin) : join($configData.reports, $repo)
+    }/`;
     $webview.loadURL(
       // <!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
-      // <style>@media print{.noprint *{display:none;height:0;}}</style></head>
+      //<style ✂prettier:content✂="QG1lZGlhIHByaW50ey5ub3ByaW50ICp7ZGlzcGxheTpub25lO2hlaWdodDowO319"></style></head>
       // <body><div id="content" contenteditable="false"><svelte></svelte></div></body></html>
       `data:text/html;charset=utf-8;base64,
       PCFET0NUWVBFIGh0bWw+PGh0bWwgbGFuZz0iZW4iPjxoZWFkPjxtZXRhIGNoYXJzZXQ9InV0Zi04
       Ij48c3R5bGU+QG1lZGlhIHByaW50ey5ub3ByaW50ICp7ZGlzcGxheTpub25lO2hlaWdodDowO319
       PC9zdHlsZT48L2hlYWQ+PGJvZHk+PGRpdiBpZD0iY29udGVudCIgY29udGVudGVkaXRhYmxlPSJm
       YWxzZSI+PHN2ZWx0ZT48L3N2ZWx0ZT48L2Rpdj48L2JvZHk+PC9odG1sPg==
-      `
-      , {baseURLForDataURL: base_url})
-    $webview.addEventListener('dom-ready', set_dokument)
+      `,
+      { baseURLForDataURL: base_url }
+    );
+    $webview.addEventListener("dom-ready", set_dokument);
   }
 
-	function startup (node) {
-    $webview = node
-    const console_message = e => {
-      console.log('%cSvelte:', 'color: blue', e.message)
-    }
-    const ipc_message = e => {
+  function startup(node) {
+    $webview = node;
+    const console_message = (e) => {
+      console.log("%cSvelte:", "color: blue", e.message);
+    };
+    const ipc_message = (e) => {
       switch (e.channel) {
-        case 'error_message': $error = e.args[0];console.log(e.args[0]); break
-        case 'dokument_options': {
-          const opts = e.args[0]
-          $kommentar = opts.kommentar
-          $pdf_name = opts.pdf_name
-          $generic_pdf = opts.generic_pdf
-          break
+        case "error_message":
+          $error = e.args[0];
+          console.log(e.args[0]);
+          break;
+        case "dokument_options": {
+          const opts = e.args[0];
+          $kommentar = opts.kommentar;
+          $pdf_name = opts.pdf_name;
+          $generic_pdf = opts.generic_pdf;
+          break;
         }
       }
-    }
-    node.addEventListener('console-message', console_message)
-    node.addEventListener('ipc-message', ipc_message)
+    };
+    node.addEventListener("console-message", console_message);
+    node.addEventListener("ipc-message", ipc_message);
     return {
-			destroy() {
-        node.removeEventListener('ipc-message', ipc_message)
-        node.removeEventListener('console-message', console_message)
-			}
-		};
-	};
+      destroy() {
+        node.removeEventListener("ipc-message", ipc_message);
+        node.removeEventListener("console-message", console_message);
+      },
+    };
+  }
 </script>
 
-<webview src="about:blank"
-         preload="./preload.js"
-         use:startup
-></webview>
+<webview src="about:blank" preload="./preload.js" use:startup />
 {#if $error}
   <div class="fehlermeldung">
     <h3 class="is-size-3">{$error.message}</h3>
@@ -134,12 +162,14 @@
 {/if}
 
 <style>
-  webview { height: -webkit-fill-available; }
+  webview {
+    height: -webkit-fill-available;
+  }
   .fehlermeldung {
     position: absolute;
     left: 250px;
     bottom: 0;
-    background: rgba(255,255,255,.9);
+    background: rgba(255, 255, 255, 0.9);
     width: 100%;
     height: 200px;
     overflow: auto;
--- src/rollup_worker.js
+++ src/rollup_worker.js
@@ -1,43 +1,32 @@
 import { expose } from "comlink";
-import { join, resolve as presolve, dirname } from 'path'
-import CheapWatch from 'cheap-watch'
-import { rollup } from 'rollup'
-import svelte from 'rollup-plugin-svelte'
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+import { join, resolve as presolve } from "path";
+import { watch } from "rollup";
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 // svelte components möchten svelte importieren. Da wir aber auch components
 // ohne node_modules zulassen, müssen wir das Verzeichnis an svelte weiterreichen
 const __nodeModules = process.env.PROD
-  ? presolve(__dirname, 'node_modules/svelte')
-  : presolve(__dirname, '../node_modules/svelte')
-
-function moduleIds (event) {
-  return {
-    name: 'rollup-plugin-module-ids',
-    buildEnd () {
-      event(this.moduleIds)
-      return null
-    }
-  }
-}
+  ? presolve(__dirname, "node_modules/svelte")
+  : presolve(__dirname, "../node_modules/svelte");
 
 class RollupBuild {
-  constructor () {
-    this.cache = null
-    this.options = null
-    this.inputOptions = null
-    this.outputOptions = null
-    this._ids = []
-    this.watcher = []
+  constructor() {
+    this.cache = null;
+    this.options = null;
+    this.inputOptions = null;
+    this.outputOptions = null;
+    this._ids = [];
+    this.watcher = null;
   }
 
-  get ids () {
-    return this._ids
+  get ids() {
+    return this._ids;
   }
-  set_options (options) {
-    this.options = options ? options : this.options
+  set_options(options) {
+    this.options = options ? options : this.options;
     this.inputOptions = {
       input: this.options.source,
       cache: this.cache,
@@ -47,70 +36,57 @@ class RollupBuild {
         json({ preferConst: true }),
         svelte({
           onwarn: (warning, handler) => {
-            if (warning.code === 'css-unused-selector') return
-            handler(warning)
+            if (warning.code === "css-unused-selector") return;
+            handler(warning);
           },
           sveltePath: __nodeModules,
           immutable: false,
           accessors: true,
-          dev: this.options.debug
+          dev: this.options.debug,
         }),
-        moduleIds(ids => this._ids = Array.from(ids)),
         resolve({ preferBuiltins: false, browser: true }),
-        commonjs()
-      ]
-    }
+        commonjs(),
+      ],
+    };
     this.outputOptions = {
-      file: join(this.options.dest, '/bundle.js'),
-      format: 'cjs',
-      name: 'components',
-      sourcemap: this.options.source_maps && 'inline'
-    }
+      file: join(this.options.dest, "/bundle.js"),
+      format: "cjs",
+      name: "components",
+      sourcemap: this.options.source_maps && "inline",
+    };
   }
-  async build () {
+  async build(cb) {
+    const watchOptions = {
+      ...this.inputOptions,
+      output: [this.outputOptions],
+      watch: {
+        skipWrite: !this.options.write,
+        exclude: "node_modules/**",
+      },
+    };
     try {
-      const bundle = await rollup(this.inputOptions)
-      this.cache = bundle.cache
-      const {output} = await bundle.generate(this.outputOptions)
-      console.log('Komponenten erfolgreich kompiliert')
-      const [compiled_module] = output
-      if (this.options.write) {
-        await bundle.write(this.outputOptions)
-        console.log('Komponenten erfolgreich geschrieben')
-      }
-      if (this.options.source_maps) {
-        compiled_module.code += `\n//# sourceMappingURL=${compiled_module.map.toUrl()}\n`;
-      }
-      return compiled_module
-    } catch (error) {
-      throw error
-    }
-  }
-  watch (cb) {
-    while (this.watcher.length) { this.watcher.pop().close() }
-    Array.from(this._ids).forEach(async (moduleID) => {
-      if (!moduleID.includes('node_modules')) {
-        const emitter = new CheapWatch({
-          dir: dirname(moduleID),
-          debounce: 30,
-          filter: ({ path, stats }) => moduleID.endsWith(path)
-        })
-        console.log('Beobachte: ' + moduleID)
-        try {
-          await emitter.init()
-          emitter.on('+', async ({ path, stats, isNew }) => {
-            if (!isNew) {
-              console.log('Änderungen bei: ' + path)
-              cb()
-            }
-          })
-        } catch (e) {
-          console.log(e)
+      this.watcher && this.watcher.close();
+      this.watcher = watch(watchOptions);
+      this.watcher.on("event", async (event) => {
+        if (event.code === "ERROR") {
+          console.log(event)
+          cb(event)
         }
-        this.watcher.push(emitter)
-      }
-    })
+        if (event.code === "BUNDLE_END") {
+          const { output } = await event.result.generate(this.outputOptions);
+          console.log("Komponenten erfolgreich kompiliert");
+          const [compiled_module] = output;
+          if (this.options.source_maps) {
+            compiled_module.code += `\n//# sourceMappingURL=${compiled_module.map.toUrl()}\n`;
+          }
+          cb(compiled_module);
+        }
+      });
+    } catch (error) {
+      console.log(error)
+      throw error;
+    }
   }
 }
 
-expose(new RollupBuild())
+expose(new RollupBuild());
