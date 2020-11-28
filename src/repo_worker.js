aa70215b9019bd340ae637d8aa0f0f0ee3007a60
hmt <dev@hmt.im>
2020-11-28 18:48:51 +0100

    lege package.json an, wenn im Reportordner keine ist.

--- src/repo_worker.js
+++ src/repo_worker.js
@@ -1,6 +1,6 @@
 import { expose } from "comlink";
 import CheapWatch from 'cheap-watch'
-import { lstatSync, readdirSync, existsSync } from 'fs'
+import { lstatSync, readdirSync, existsSync, closeSync, openSync } from 'fs'
 import { join, basename } from 'path'
 
 function scanSource (report_location) {
@@ -13,6 +13,12 @@ function scanSource (report_location) {
         ...o,
         [basename(element)]: readdirSync(element).filter(fn => /\.((html)|(svelte))$/.test(fn) && fn.charAt(0) !== '_')
       }), {})
+    
+    Object.keys(obj).forEach(e=> {
+      if (!existsSync(join(report_location, e, 'package.json'))) {
+        closeSync(openSync(join(report_location, e, 'package.json'), 'a'))
+      }
+    })
     return obj
   } catch (e) {
     console.log('Fehler beim Scannen des Report-Verzeichnisses: ', e)
