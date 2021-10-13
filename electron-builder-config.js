9270b75ac31b090f38e8676d5e0af453fc748da6
Easton Pillay <easton@planeteaston.com>
2021-10-13 10:41:37 -0500

    Changed name of electron-builder config.

---
+++ electron-builder-config.js
@@ -0,0 +1,55 @@
+const config = {
+    appId: "im.hmt.report.schild",
+    productName: "schild.report",
+    artifactName: "schild.report.${ext}",
+    buildVersion: `2.5.${process.env.GITHUB_RUN_NUMBER}`,
+    copyright: `Copyright ${process.env.THE_AUTHOR}`,
+    extraMetadata: {
+        author: {
+            name: `${process.env.THE_AUTHOR}`,
+            email: `${process.env.THE_EMAIL}`
+        }
+    },
+    files: [
+        "./build/**/*"
+    ],
+    win: {
+        icon: "icons/icon.ico",
+        target: "msi"
+    },
+    linux: {
+        icon: "icons/icon.png",
+        category: "app.tools",
+        executableName: "schild.report",
+        target: [
+            "AppImage",
+            "snap",
+            "deb",
+            "rpm"
+        ]
+    },
+    mac: {
+        icon: "icons/icon.icns",
+        target: "dmg"
+    },
+    dmg: {
+        icon: "icons/icon.icns",
+        contents: [
+            {
+                x: 130,
+                y: 220
+            },
+            {
+                x: 550,
+                y: 22,
+                type: "link",
+                path: "/Applications"
+            }
+        ],
+        window: {
+            width: 680,
+            height: 42
+        }
+    }
+}
+module.exports = config
